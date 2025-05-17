aaff2b5a6447ae778ace0aa2cf8d04295cb239a7
GouthamSomaraju <mr.goutham00@gmail.com>
2025-05-17 15:01:48 +0530

    Nodejs system utility toolkit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+# Node.JS task - Day1
 # Node.js-System-Utility-Toolkit
-Node.js System Utility Toolkit
+
+
+# 🛒 JavaScript Task: Mini Shopping Cart Simulation
+
+![CODE OUTPUT:](./day1.png)
+
+![Cart Page](./assets/cart.png)
---
+++ system.js
@@ -0,0 +1,19 @@
+let chalk=require('chalk')
+let path=require('path')
+let os=require('os')
+
+let { greet } = require('./greet')
+
+console.log(chalk.green('System Info using CLI'));
+
+
+console.log('Current Dictionary:',__dirname);
+console.log('Current File:', __filename);
+console.log('ENV Mode:',process.env.NODE_ENV||'Development');
+
+
+console.log(chalk.yellow(greet('Goutham')));
+
+
+
+
