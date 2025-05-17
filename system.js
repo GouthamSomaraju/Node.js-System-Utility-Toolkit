4ca385a4e45ca9c45bcb943735f35baca675e874
GouthamSomaraju <mr.goutham00@gmail.com>
2025-05-17 15:10:16 +0530

    Node.js-System-Utility-Toolkit

--- system.js
+++ system.js
@@ -6,9 +6,12 @@ let { greet } = require('./greet')
 
 console.log(chalk.green('System Info using CLI'));
 
+console.log('Operating System:', os.type());
+console.log('Total Memory (MB):', os.totalmem() / (1024 * 1024));
+
 
 console.log('Current Dictionary:',__dirname);
-console.log('Current File:', __filename);
+console.log('Current File:      ', __filename);
 console.log('ENV Mode:',process.env.NODE_ENV||'Development');
 
 
